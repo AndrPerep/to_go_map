76c1a1d14af96fef292c61d5d8ed67bc73a9e807
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-11-12 15:29:40 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from json import load
+# def index(request):
+#     with open('./places.json', 'r') as file:
+#         places = load(file)
+#     # places = CityProject[0]
+#     data = {'places': places}
+#     return render(request, 'index.html', context=data)
+
