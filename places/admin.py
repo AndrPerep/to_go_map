fd8d742ca6853c418a683887e8836689a09c24d1
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-12-03 16:22:44 +0300

    use inlines for Image

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from places.models import CityProject, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(CityProject)
 class CityProjectAdmin(admin.ModelAdmin):
     list_display = ['title']
+    inlines = [ImageInline]
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['id', 'city_project']
+
