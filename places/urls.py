b6fdcc95873086cd077a827f0e8f8b7f87135e5a
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2023-03-07 16:15:06 +0300

    Update urls.py

--- places/urls.py
+++ places/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.show_map),
-    path('places/<int:place_id>', views.show_place, name='show_place'),
+    path('places/<int:place_id>/', views.show_place, name='show_place'),
 ]
